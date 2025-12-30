22589d41225ed4417fed55acb1abdcfd5612c5c1
Ana Lemos <carol1692@hotmail.com>
2025-12-30 02:01:27 -0300

    insert comments and adjust reponsivity xl and lg on skills page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,21 +8,22 @@ export default function Home() {
   	return (
     <Layout>
         <div className="w-full h-screen flex flex-col justify-between content-around">
-				<div>
-					<div className='flex justify-end pt-1 pb-1 gap-0.5 pe-1 border-2 text-(--color-desert-sand-800) border-s-2 border-end-2 border-t-2'>
-						<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-baseline-last'>
-							<MinusIcon size={22} />
-						</div>
-						<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-center'>
-							<RectangleIcon size={22} />
-						</div>
-						
-						<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-center'>
-							<XIcon size={22} />
-						</div>
+			<div>
+				{/* window header */}
+				<div className='flex justify-end pt-1 pb-1 gap-0.5 pe-1 border-2 text-(--color-desert-sand-800) border-s-2 border-end-2 border-t-2'>
+					<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-baseline-last'>
+						<MinusIcon size={22} />
+					</div>
+					<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-center'>
+						<RectangleIcon size={22} />
 					</div>
 					
-					{/* <hr className="w-full text-(--color-desert-sand-700) border"/> */}
+					<div className='flex w-8 h-8 bg-white border-2 rounded justify-center items-center'>
+						<XIcon size={22} />
+					</div>
+				</div>
+				{/* end window header */}
+				{/* home */}
 				<section className='flex h-full bg-white text-(--color-desert-sand-800) border-s-2 border-e-2 border-b-2 overflow-hidden' >
 					<div className='flex flex-col w-full xl:gap-4 xl:mt-4'>
 						<section className='flex flex-col w-full xl:p-2 xl:h-1/3 xl:gap-3 lg:p-1 lg:h-1/4 lg:gap-2'>
@@ -40,10 +41,9 @@ export default function Home() {
 								<img src="https://ik.imagekit.io/5vd9mfrfv/img/char_redimensionado.gif" className="absolute bottom-0 left-1/2 -translate-x-1/2 xl:h-full lg:h-[90%]" alt="cute girl waving" />
 							</section>
 						</div>
-						
 					</div>
 				</section> 
-
+				{/* tools used */}
 				<section className='flex flex-col text-(--color-desert-sand-800) bg-white/70 xl:mt-6 lg:mt-4 '>
 						<section>
 							<h1 className="montserrat-font text-sm p-1">This web page was created using:</h1>
@@ -56,8 +56,8 @@ export default function Home() {
 							<IconesTooltip urlIcon={`https://img.icons8.com/?size=${iconSize}&id=uY6QHPV8g5bd&format=png&color=000000`} name={'Google Fonts'} />
 						</section>
 				</section>
-				</div>
 			</div>
+		</div>
     </Layout>
     
   )
